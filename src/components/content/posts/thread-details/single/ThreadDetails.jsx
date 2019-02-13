e5dd5d2a764ca7addf5c42fc91c755a405641986
Todor Ivanov <todor.ivanov@primeholding.com>
2019-02-13 18:32:20 +0200

    Add basic thread details components

---
+++ src/components/content/posts/thread-details/single/ThreadDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class ThreadDetails extends React.Component {
+    render() {
+        return (
+            <div className="thread-details-holder">
+            </div>
+        );
+    }
+}
