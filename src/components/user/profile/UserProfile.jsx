43af4fd3f74fc0209d69dc7aa51065dc826492de
Todor Ivanov <ivanov.todor1@gmail.com>
2019-03-03 09:37:31 +0200

    Add functionallity to UserProfile component

--- src/components/user/profile/UserProfile.jsx
+++ src/components/user/profile/UserProfile.jsx
@@ -2,7 +2,35 @@
 import React from 'react';
 import './UserProfile.scss';
 import { Media } from 'reactstrap';
+import UsersService from './../../../core/services/users.service';
 export class UserProfile extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: '',
+            lastName: '',
+            username: '',
+            password: '',
+            re_password: '',
+            email: ''
+        };
+    }
+
+    onChange(event) {
+        event.persist();
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        UsersService.save(this.state).then(() => {
+            this.props.history.push('/home');
+        });
+    }
 
     render() {
         return (
@@ -20,20 +48,28 @@ export class UserProfile extends React.Component {
                         </div> 
                         <div className="col-10">
                             <form>
-                                <div className="row mb-2">
+                            <div className="row mb-2">
                                     <div className="col-6">
-                                        <input type="text" name="firstName" id="firstName" placeholder="First Name" />
+                                        <input type="text" name="username" id="username" placeholder="Username" onChange={this.onChange.bind(this)} required/>
                                     </div>
                                     <div className="col-6">
-                                        <input type="text" name="lastName" id="lastName" placeholder="Last Name" />
+                                        <input type="email" name="email" id="email" placeholder="Email" onChange={this.onChange.bind(this)} required/>
+                                    </div>
+                                </div>
+                                <div className="row mb-2">
+                                    <div className="col-6">
+                                        <input type="text" name="firstName" id="firstName" placeholder="First Name" onChange={this.onChange.bind(this)} required/>
+                                    </div>
+                                    <div className="col-6">
+                                        <input type="text" name="lastName" id="lastName" placeholder="Last Name" onChange={this.onChange.bind(this)} required/>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-6">
-                                        <input type="password" name="password" id="password" placeholder="Password" />
+                                        <input type="password" name="password" id="password" placeholder="Password" onChange={this.onChange.bind(this)} required/>
                                     </div>
                                     <div className="col-6">
-                                        <input type="password" name="re_password" id="rePassword" placeholder="Re-type password" />
+                                        <input type="password" name="re_password" id="rePassword" placeholder="Re-type password" onChange={this.onChange.bind(this)} required/>
                                     </div>
                                 </div>
                             </form>
