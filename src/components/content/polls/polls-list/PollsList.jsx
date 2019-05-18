53ea195fa2f1cf4f2b564265d3f3baa019fa7cee
Todor Ivanov <todor.ivanov@primeholding.com>
2019-05-18 10:24:24 +0300

    Add tooltips on polls

--- src/components/content/polls/polls-list/PollsList.jsx
+++ src/components/content/polls/polls-list/PollsList.jsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPen, faTrashAlt, faPoll } from '@fortawesome/free-solid-svg-icons';
 import history from './../../../../core/history/History';
 import UsersService from './../../../../core/services/users.service';
+import { UncontrolledTooltip } from 'reactstrap';
 
 export class PollsList extends React.Component {
 
@@ -48,48 +49,47 @@ export class PollsList extends React.Component {
 
     onMakePollOfTheWeek(pollId) {
         PollsService.makePollOfTheWeek(pollId).then(() => {
-            
+
         });
-
-        for(let i=0; i < 10; i++) {
-            PollsService.addVote({
-                pollId: pollId,
-                userId: UsersService.getLoggedUserId(),
-                answerIndex: 0
-            }).then(() => {
-
-            });
-        }
     }
 
     render() {
         const polls = this.state.polls.map(poll => {
             return (
                 <div className="list-item">
-                        <div className="row">
-                            <div className="col-10 p-3">
-                                <h4 className="ml-3 cursor-pointer">{poll.question}</h4>
-                            </div>
-                            <div className="col-2 d-flex align-items-center justify-content-center text-right">
-                                <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faPen} onClick={() => this.onEditPoll(poll.id)}/></span>
-                                <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faTrashAlt} onClick={() => this.onDeletePoll(poll.id)} /></span>
-                                <span className="cursor-pointer"><FontAwesomeIcon icon={faPoll} onClick={() => this.onMakePollOfTheWeek(poll.id)} /></span>
-                            </div>
+                    <div className="row">
+                        <div className="col-10 p-3">
+                            <h4 className="ml-3 cursor-pointer">{poll.question}</h4>
+                        </div>
+                        <div className="col-2 d-flex align-items-center justify-content-center text-right">
+                            <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faPen} onClick={() => this.onEditPoll(poll.id)} id="edit-btn"/></span>
+                            <UncontrolledTooltip placement="top" target="edit-btn">
+                                Edit
+                            </UncontrolledTooltip>
+                            <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faTrashAlt} onClick={() => this.onDeletePoll(poll.id)} id="delete-btn"/></span>
+                            <UncontrolledTooltip placement="top" target="delete-btn">
+                                Delete
+                            </UncontrolledTooltip>
+                            <span className="cursor-pointer"><FontAwesomeIcon icon={faPoll} onClick={() => this.onMakePollOfTheWeek(poll.id)} id="poll-of-the-week"/></span>
+                            <UncontrolledTooltip placement="top" target="poll-of-the-week">
+                                Make poll of the week
+                            </UncontrolledTooltip>
                         </div>
                     </div>
+                </div>
             );
         });
 
         return (
             <div className="polls-list">
-            <div className="row">
-                <div className="col-12 text-right mb-3">
-                <button className="create-btn btn btn-primary" onClick={this.onAddPoll.bind(this)}>Add Poll</button>
+                <div className="row">
+                    <div className="col-12 text-right mb-3">
+                        <button className="create-btn btn btn-primary" onClick={this.onAddPoll.bind(this)}>Add Poll</button>
+                    </div>
                 </div>
-            </div>                
                 <div className="list-hoder">
                     {polls}
-                </div>                
+                </div>
             </div>
         );
     }
