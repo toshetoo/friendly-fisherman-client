75033ec270ed8b3a1be5b28800c1254357252cf9
Todor Ivanov <todor.ivanov@primeholding.com>
2019-05-19 10:37:47 +0300

    Highlight current Poll Of The Week

--- src/components/content/polls/polls-list/PollsList.jsx
+++ src/components/content/polls/polls-list/PollsList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './PollsList.scss';
 import PollsService from '../../../../core/services/polls.service';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen, faTrashAlt, faPoll } from '@fortawesome/free-solid-svg-icons';
+import { faPen, faTrashAlt, faPoll, faVoteYea } from '@fortawesome/free-solid-svg-icons';
 import history from './../../../../core/history/History';
 import { UncontrolledTooltip } from 'reactstrap';
 
@@ -48,7 +48,16 @@ export class PollsList extends React.Component {
 
     onMakePollOfTheWeek(pollId) {
         PollsService.makePollOfTheWeek(pollId).then(() => {
+            const polls = this.state.polls;
+            let oldPoll = polls.find(p => p.isPollOfTheWeek);
+            oldPoll.isPollOfTheWeek = false;
 
+            let newPoll = polls.find(p => p.id === pollId);
+            newPoll.isPollOfTheWeek = true;
+
+            this.setState({
+                polls: polls
+            });
         });
     }
 
@@ -58,21 +67,36 @@ export class PollsList extends React.Component {
                 <div className="list-item" key={poll.id}>
                     <div className="row">
                         <div className="col-10 p-3">
-                            <h4 className="ml-3 cursor-pointer">{poll.question}</h4>
+                            {poll.isPollOfTheWeek ? 
+                                (<span className="ml-3">
+                                    <FontAwesomeIcon icon={faVoteYea} id="current-pow"></FontAwesomeIcon>
+                                    <UncontrolledTooltip placement="top" target="current-pow">
+                                        Current Poll Of The Week
+                                    </UncontrolledTooltip>
+                                </span>
+                                ): '' }                            
+                            
+                            
+                            <h4 className="ml-3 cursor-pointer d-inline-block">{poll.question}</h4>
                         </div>
                         <div className="col-2 d-flex align-items-center justify-content-center text-right">
-                            <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faPen} onClick={() => this.onEditPoll(poll.id)} id="edit-btn"/></span>
+                            <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faPen} onClick={() => this.onEditPoll(poll.id)} id="edit-btn" /></span>
                             <UncontrolledTooltip placement="top" target="edit-btn">
                                 Edit
                             </UncontrolledTooltip>
-                            <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faTrashAlt} onClick={() => this.onDeletePoll(poll.id)} id="delete-btn"/></span>
+                            <span className="cursor-pointer mr-2"><FontAwesomeIcon icon={faTrashAlt} onClick={() => this.onDeletePoll(poll.id)} id="delete-btn" /></span>
                             <UncontrolledTooltip placement="top" target="delete-btn">
                                 Delete
                             </UncontrolledTooltip>
-                            <span className="cursor-pointer"><FontAwesomeIcon icon={faPoll} onClick={() => this.onMakePollOfTheWeek(poll.id)} id="poll-of-the-week"/></span>
-                            <UncontrolledTooltip placement="top" target="poll-of-the-week">
-                                Make poll of the week
-                            </UncontrolledTooltip>
+                            
+                            {!poll.isPollOfTheWeek ? (
+                                <span>
+                                    <span className="cursor-pointer"><FontAwesomeIcon icon={faPoll} onClick={() => this.onMakePollOfTheWeek(poll.id)} id="poll-of-the-week" /></span>
+                                    <UncontrolledTooltip placement="top" target="poll-of-the-week">
+                                        Make poll of the week
+                                    </UncontrolledTooltip>
+                                </span>
+                            ) : ''}
                         </div>
                     </div>
                 </div>
