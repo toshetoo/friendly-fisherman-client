7dbe5b5df64518afe3c2f17394d4f0b514a131ae
Todor Ivanov <ivanov.todor1@gmail.com>
2019-06-11 00:37:14 +0300

    Add notifications

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { InputGroup, InputGroupAddon, Button, Input, UncontrolledDropdown, UncontrolledTooltip, DropdownToggle, DropdownMenu, Row, Col } from "reactstrap";
+import { InputGroup, InputGroupAddon, Button, Input, UncontrolledDropdown, DropdownItem, UncontrolledTooltip, DropdownToggle, DropdownMenu, Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faEnvelope, faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faEnvelope, faSearchPlus, faBell } from "@fortawesome/free-solid-svg-icons";
 import "./Header.scss";
 import UsersService from './../../../core/services/users.service';
 import MessagesService from './../../../core/services/messages.service';
@@ -84,7 +84,7 @@ export class Header extends React.Component {
 
         const imageSrc = () => {
             if (this.state.imagePath !== undefined) {
-                return API_BASE+ this.state.imagePath;
+                return API_BASE + this.state.imagePath;
             } else {
                 return NO_IMAGE_URL;
             }
@@ -123,15 +123,82 @@ export class Header extends React.Component {
                                 </InputGroupAddon>
                             </InputGroup>
                             {isLoggedIn ?
-                            <Link to="/create-topic"><button className="create-topic-btn">Create topic</button></Link>
-                            : ''}
-                             {isLoggedIn ?
-                            <Link to="/add-event"><button className="create-topic-btn">Add event</button></Link>
-                            : ''}
-                            
+                                <Link to="/create-topic"><button className="create-topic-btn">Create topic</button></Link>
+                                : ''}
+                            {isLoggedIn ?
+                                <Link to="/add-event"><button className="create-topic-btn">Add event</button></Link>
+                                : ''}
+
                         </div>
                         {isLoggedIn ?
                             <div className="col-2 pl-0 ml-0 d-flex justify-content-end align-items-center">
+                                <span className="icon-holder text-muted">
+                                    <span className="number-of-notifications">{3}</span>
+
+                                    <UncontrolledDropdown className="notifications-toggle">
+                                        <DropdownToggle caret>
+                                            <span><a href="#" ><FontAwesomeIcon icon={faBell} color={messageNotificationColor} /></a></span>
+                                        </DropdownToggle>
+                                        <DropdownMenu>
+                                        <DropdownItem divider />
+                                            <DropdownItem>
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <div class="media-object">
+                                                            <img data-src="holder.js/50x50?bg=cccccc" class="img-circle" alt="50x50" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2250%22%20height%3D%2250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2050%2050%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b4341eb03%20text%20%7B%20fill%3A%23919191%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b4341eb03%22%3E%3Crect%20width%3D%2250%22%20height%3D%2250%22%20fill%3D%22%23cccccc%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%226.4765625%22%20y%3D%2229.55625%22%3E50x50%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <strong class="notification-title"><a href="#">Dave Lister</a> commented on <a href="#">DWARF-13 - Maintenance</a></strong>
+                                                        <p class="notification-desc">I totally don't wanna do it. Rimmer can do it.</p>
+
+                                                        <div class="notification-meta">
+                                                            <small class="timestamp">27. 11. 2015, 15:00</small>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </DropdownItem>
+                                            <DropdownItem divider />
+                                            <DropdownItem>
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <div class="media-object">
+                                                            <img data-src="holder.js/50x50?bg=cccccc" class="img-circle" alt="50x50" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2250%22%20height%3D%2250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2050%2050%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b4341eb06%20text%20%7B%20fill%3A%23919191%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b4341eb06%22%3E%3Crect%20width%3D%2250%22%20height%3D%2250%22%20fill%3D%22%23cccccc%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%226.4765625%22%20y%3D%2229.55625%22%3E50x50%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <strong class="notification-title"><a href="#">Nikola Tesla</a> resolved <a href="#">T-14 - Awesome stuff</a></strong>
+
+                                                        <p class="notification-desc">Resolution: Fixed, Work log: 4h</p>
+
+                                                        <div class="notification-meta">
+                                                            <small class="timestamp">27. 10. 2015, 08:00</small>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </DropdownItem>
+                                            <DropdownItem divider />
+                                            <DropdownItem>
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <div class="media-object">
+                                                            <img data-src="holder.js/50x50?bg=cccccc" class="img-circle" alt="50x50" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%2250%22%20height%3D%2250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2050%2050%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b4341eb09%20text%20%7B%20fill%3A%23919191%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A10pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b4341eb09%22%3E%3Crect%20width%3D%2250%22%20height%3D%2250%22%20fill%3D%22%23cccccc%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%226.4765625%22%20y%3D%2229.55625%22%3E50x50%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <strong class="notification-title"><a href="#">James Bond</a> resolved <a href="#">B-007 - Desolve Spectre organization</a></strong>
+
+                                                        <div class="notification-meta">
+                                                            <small class="timestamp">1. 9. 2015, 08:00</small>
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+                                            </DropdownItem>
+                                        </DropdownMenu>
+                                    </UncontrolledDropdown>
+                                </span>
                                 <span className="icon-holder mr-2 text-muted">
                                     <span className="number-of-messages" hidden={numberOfMessages === 0}>{numberOfMessages}</span>
                                     <Link to="/messages"><FontAwesomeIcon icon={faEnvelope} color={messageNotificationColor} /></Link>
